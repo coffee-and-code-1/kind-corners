277b1849642dcbbce288bcc3e2248bddd93b71cd
coffee-and-code-1 <86799717+coffee-and-code-1@users.noreply.github.com>
2021-11-26 08:53:20 +0800

    First twitter scrape

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -1,22 +1,39 @@
-# review how to create a virutal environment so that you don't download information permanently
-# this took me a long time to create because of the following bottlenecks
+# review virtual env 
+
+# this took me a long time initially b/c of the following bottlenecks 
     # needed to upgrade my Twitter subscription
-    # install pip tweepy -- here I have a question of when do you know you need to pip install AND import in the file?
-    # when do you pip install X and not need to import, or import in the python file but not need to pip install? 
+    # install pip tweepy -- how do you know when you need to pip install + import file, vs only import file (no pip install)
 
 import tweepy
 import csv
 import ssl
-import pandas
+import pandas as pd 
 
+
+#import nltk
+#nltk.download('punkt')
+#nltk.download('wordnet')
+#from nltk import sent_tokenize, word_tokenize
+#from nltk.stem.snowball import SnowballStemmer
+#from nltk.stem.wordnet import WordNetLemmatizer
+#from nltk.corpus import stopwords
+
+import numpy as np
+import re  
+
+#import spacy
+#nlp = spacy.load('en_core_web_lg')
+
+#from twitterscraper import query_tweets
+#from twitterscraper.query import query_tweets_from_user
+#import datetime as dt 
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-
-consumer_key = "JMFq9HN0USGrqjvtA8ZEBLHTT"
-consumer_secret = "OlWgEZywRwYMdtGrm94z4WPtg9Hk5TQURXYntl6bsOPTMTE7eU"
-access_token = "1462717268496842752-e3yRRPID1BfRmp0TsWEjEby0f7Tkbq"
-access_token_secret = "bgpXRDDrOrfYq9FL5RKYz86xAkETguyEevKyTVRRrYX0M"
+consumer_key = " "
+consumer_secret = " "
+access_token = " "
+access_token_secret = " " 
 
 def get_all_tweets(screen_name):
 
@@ -25,7 +42,6 @@ def get_all_tweets(screen_name):
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
 #print(tweepy.__version__)
-
 
 # extract the latest 200 tweets using api.user_timeline
 
@@ -56,5 +72,31 @@ def get_all_tweets(screen_name):
         writer = csv.writer(f)
         writer.writerow(["id","created at","text","location","retweet count"])
         writer.writerows(outtweets)
+        
+    df=pd.read_csv("new_{}_tweets.csv".format(screen_name))
+    print(df)
+    # this is ok for now -- but can we find a better way to put all of the tweets in one file instead of multiple CSVs? 
     
+    #ds_tweets=pd.DataFrame(t.__dict__ for ds in ds_tweets)
+    #print(ds_tweets)
+
+
 get_all_tweets("DalaiLama")
+
+# eventually you want to automatically add people to the inspirational_individuals list if they show up a certain % of times in someone's retweets 
+
+#for individual in inspirational_individuals:
+    #get_all_tweets(individual)
+   
+
+# --- exercise #1 -- isolate the top words in the tweets when you remove the "in", "and", "etc." 
+
+# to do this, you  need to isolate a dataframe for the text itself
+#ds_tweets = pd.DataFrame(t.__dict__ for ds in ds_tweets)
+#print(ds_tweets)
+
+# ----- other exercises to be filled in
+# top liked tweets 
+# retweets, so that you can find additional twitter users to follow (follow your inspiration's inspiration) 
+# region 
+# words that are commonly used as in the same sentence as the search words you're initially looking for
